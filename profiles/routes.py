73b73665e6dea2ede465e41e1cb818a904c4205d
ANSAR <ansar@mail.ru>
2026-05-25 17:51:34 +0300

    Добавили изменение почты профиля

--- profiles/routes.py
+++ profiles/routes.py
@@ -25,7 +25,20 @@ def change_username():
     return render_template('change_username.html')
 
 
+@profile_bp.route('/change-email', methods=['GET', 'POST'])
+def change_email():
+    if request.method == 'POST':
+        new_email = request.form.get('new-email')
+        try:
+            if new_email and len(new_email) >= 3:
+                current_user.email = new_email
+                db.session.commit()
+                return redirect(url_for('profile.profile'))
+        except Exception:
+            print('Такая почта существует!')
+            return redirect(url_for('profile.change_email'))
 
+    return render_template('change_email.html')
 
 
 
